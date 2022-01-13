6ba8488de17c9cc38247d80744b1408a1996b92b
Tomasz Kaczkowski <tomasz.kaczkowski@volue.com>
2022-01-13 08:30:47 +0100

    Test

--- index.js
+++ index.js
@@ -2,52 +2,51 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const { context, GitHub } = require('@actions/github/lib/utils');
 
-try {
-  const input = core.getInput('relese-notes-ignore-pattern')
+async function run() {
+  try {
+    const input = core.getInput('relese-notes-ignore-pattern')
 
-  const client = new GitHub({auth: core.getInput('token', { required: true })})
-  
-  console.log(github.context.payload)
-  console.log(github.context.eventName)
-  
-  switch (context.eventName) {
-    case 'pull_request':
-      base = context.payload.pull_request?.base?.sha
-      head = context.payload.pull_request?.head?.sha
-      break
-    case 'push':
-      base = context.payload.before
-      head = context.payload.after
-      break
-    default:
-      throw "Only push and pull_request is supported"
-  }
+    const client = new GitHub({ auth: core.getInput('token', { required: true }) })
 
-  console.log(base)
-  console.log(head)
+    console.log(github.context.payload)
+    console.log(github.context.eventName)
 
-  response = (async function(client){
-    const response = await client.rest.repos.compareCommits({
-      base,
-      head,
-      owner: github.context.repo.owner,
-      repo: github.context.repo.repo
-    })
+    switch (context.eventName) {
+      case 'pull_request':
+        base = context.payload.pull_request?.base?.sha
+        head = context.payload.pull_request?.head?.sha
+        break
+      case 'push':
+        base = context.payload.before
+        head = context.payload.after
+        break
+      default:
+        throw "Only push and pull_request is supported"
+    }
+
+    console.log(base)
+    console.log(head)
+
+      const response = await client.rest.repos.compareCommits({
+        base,
+        head,
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo
+      })
+
+    console.log("Response:")
     console.log(response)
-    return response
-  })(client)
+    const changedFiles = response.data.files
 
-  let result = await Promise.all([response])
-  console.log("Response:")
-  console.log(result)
-  const changedFiles = response.data.files
+    console.log(changedFiles)
 
-  console.log(changedFiles)
-
-  const pullRequestBody = github.context.payload.pull_request?.body
-  if (!pullRequestBody.includes(input)) {
-    throw new Error(`Must put ${input} in PR description`)
+    const pullRequestBody = github.context.payload.pull_request?.body
+    if (!pullRequestBody.includes(input)) {
+      throw new Error(`Must put ${input} in PR description`)
+    }
+  } catch (error) {
+    core.setFailed(error.message);
   }
-} catch (error) {
-  core.setFailed(error.message);
 }
+
+run()
