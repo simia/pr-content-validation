c23629c9ed5ee3012fae387d7a4a7c0a78b87616
Tomasz Kaczkowski <tomasz.kaczkowski@volue.com>
2022-01-13 10:47:17 +0100

    Test

--- index.js
+++ index.js
@@ -8,42 +8,12 @@ async function run() {
     const releaseNotesFilename = core.getInput('release-notes-file')
     const client = new GitHub({ auth: core.getInput('token', { required: true }) })
     
-    const request = {
-      owner: context.repo.owner,
-      repo: context.repo.repo,
-      pull_number: context.issue.number
-    }
-    console.log(`Getting PR #${request.pull_number} from ${request.owner}/${request.repo}`)
-
-      const result = await client.rest.pulls.get(request)
-
-    console.log(result)
-    client.rest.checks.create({
-      owner: context.repo.owner,
-      repo: context.repo.repo,
-      name: "Check Created by API",
-      head_sha: result.data.head.sha,
-      status: "completed",
-      conclusion: "success",
-      output: {
-        title: "Check Created by API",
-        summary: `dupa`
-      }
-    })
-    //console.log(result)
-
-    // console.log(context.payload.issue?.pull_request)
-    // console.log(context.payload.pull_request)
-
     switch (context.eventName) {
       case 'pull_request':
       case 'pull_request_review_comment':
         base = context.payload.pull_request?.base?.sha
         head = context.payload.pull_request?.head?.sha
         break
-      case 'issue':
-        base = context.payload.issue.pull_request?.base?.sha
-        head = context.payload.issue.pull_request?.head?.sha
       default:
         throw "Only push and pull_request is supported"
     }
@@ -61,7 +31,6 @@ async function run() {
       return
     }
     
-    console.log(context.payload.comment)
     const pullRequestBody = github.context.payload.pull_request?.body
     if (!pullRequestBody.includes(input)) {
       throw new Error(`Must put \"${input}\" in PR description or update release notes file \"${releaseNotesFilename}\"`)
