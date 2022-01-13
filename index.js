feca21d62a2268cec8b160a14cd038e0d9792747
Tomasz Kaczkowski <tomasz.kaczkowski@volue.com>
2022-01-13 09:46:03 +0100

    Test

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ async function run() {
     const releaseNotesFilename = core.getInput('release-notes-file')
     const client = new GitHub({ auth: core.getInput('token', { required: true }) })
 
-    console.log(context.payload.issue.pull_request)
+    console.log(context.payload.issue?.pull_request)
     console.log(context.payload.pull_request)
 
     switch (context.eventName) {
